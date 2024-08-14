7bbd5393308498bd0694a3c9e24e3795206cb226
Vishal Mullur <vishal@anomalytheory.com>
2024-08-14 14:09:16 +0530

    Function adds multiple numbers

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,3 +3,7 @@ import { add } from './index';
 test('Function adds two numbers from a string', () => {
   expect(add('1,2')).toBe(3);
 })
+
+test('Function adds multiple numbers', () => {
+  expect(add('1,2,3,4,5')).toBe(15);
+})
