3b6e390c8e7b5f4c1fdb815e44486b5aa10db91e
Vishal Mullur <vmullur-v@ejada.com>
2025-03-24 00:01:33 +0530

    Pass test: Single number returns same number

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ export const add = (numbers?: string) => {
   if (!numbers || numbers.length <= 0) {
     return 0;
   }
+
+  const numbersArray = numbers.split(',');
+  if (numbersArray.length === 1) {
+    return Number(numbersArray[0]);
+  }
 }
