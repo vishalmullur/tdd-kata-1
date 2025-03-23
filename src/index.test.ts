43de67c347c6cf2b52e58bd43efd977a34687d43
Vishal Mullur <vmullur-v@ejada.com>
2025-03-23 23:54:21 +0530

    Add test: input no argumets

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,5 +3,9 @@ import { add } from "./index";
 describe('Add numbers test', () => {
   test('Input empty string', () => {
     expect(add('')).toBe(0);
+  });
+
+  test('Input no arguments', () => {
+    expect(add()).toBe(0);
   })
 })
