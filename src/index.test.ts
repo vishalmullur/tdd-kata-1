c6a972e8d802dada7c691f6a80147ae952637890
Vishal Mullur <vmullur-v@ejada.com>
2025-03-24 00:08:57 +0530

    Add test: Input unknown amount of numbers returns sum; Test passes

--- src/index.test.ts
+++ src/index.test.ts
@@ -20,4 +20,8 @@ describe('Add numbers test', () => {
   test('Input only delimieter returns 0', () => {
     expect(add(',')).toBe(0);
   });
+
+  test('Input unknown amount of numbers returns sum', () => {
+    expect(add('1,2,3,4,5,6')).toBe(21);
+  })
 })
