2a0f29ab295d684045ca4cd601429b186c0de113
Vishal Mullur <vmullur-v@ejada.com>
2025-03-24 01:14:00 +0530

    Add test: negative number throws error

--- src/index.test.ts
+++ src/index.test.ts
@@ -31,5 +31,9 @@ describe('Add numbers test', () => {
 
   test('Input custom delimiter returns sum', () => {
     expect(add("//;\n1;2;3")).toBe(6);
-  })
+  });
+
+  test('Input negative numbers throws error', () => {
+    expect(() => add("-1")).toThrow(new Error('Negatives not allowed; -1'))
+  });
 })
