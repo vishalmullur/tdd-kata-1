7930de989722c92f03a3cd4f5a1dfd386ccd82f2
Vishal Mullur <vmullur-v@ejada.com>
2025-03-24 01:42:13 +0530

    Pass test: input delimiter of any length

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,19 @@ export const add = (numbers?: string) => {
 
   let numbersArray;
 
-  // For customer delimiters, the string 'numbers' starts with '//'
+  // For custom delimiters, the string 'numbers' starts with '//'
   if (numbers.startsWith('//')) {
     const stringParts = numbers.split('\n');
-    const delimieter = stringParts[0].slice(2, 3);
+    let delimieter;
+
+    // When delimiter includes multiple chars, it is enclosed within square brackets
+    // Hence, we start slice at 3 (//[) and end with 'length -1' (]) to find the actual chars in between
+    if (stringParts[0].endsWith(']')) {
+      delimieter = stringParts[0].slice(3, stringParts[0].length - 1);
+    } else {
+      // If delimieter is not enclosed within square brackets, the delimieter is a single char
+      delimieter = stringParts[0].slice(2, 3);
+    }
 
     numbersArray = stringParts[1].split(delimieter);
   } else {
